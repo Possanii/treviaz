4e4371070945d9d6fcf8dfe5d83393c04a973681
Possanii <gpossanii@outlook.com>
2024-09-16 20:35:44 -0300

    feat: login page ui

---
+++ apps/web/src/app/(app)/auth/sign-in/page.tsx
@@ -0,0 +1,63 @@
+import { Button } from '@treviaz/ui/components/ui/button'
+import { Input } from '@treviaz/ui/components/ui/input'
+import { Label } from '@treviaz/ui/components/ui/label'
+import Image from 'next/image'
+import Link from 'next/link'
+
+export default function SignInPage() {
+  return (
+    <div className="w-full lg:grid lg:min-h-[600px] lg:grid-cols-2 xl:min-h-screen">
+      <div className="flex items-center justify-center py-12">
+        <div className="mx-auto grid w-[350px] gap-6">
+          <div className="grid gap-2 text-center">
+            <h1 className="text-3xl font-bold">Login</h1>
+            <p className="text-muted-foreground text-balance">
+              Entre com seu email para acessar sua conta
+            </p>
+          </div>
+          <div className="grid gap-4">
+            <div className="grid gap-2">
+              <Label htmlFor="email">Email</Label>
+              <Input
+                id="email"
+                type="email"
+                placeholder="m@example.com"
+                required
+              />
+            </div>
+            <div className="grid gap-2">
+              <div className="flex items-center">
+                <Label htmlFor="password">Senha</Label>
+                <Link
+                  href="/forgot-password"
+                  className="ml-auto inline-block text-sm underline"
+                >
+                  Esqueceu sua senha?
+                </Link>
+              </div>
+              <Input id="password" type="password" required />
+            </div>
+            <Button type="submit" className="w-full">
+              Login
+            </Button>
+          </div>
+          <div className="mt-4 text-center text-sm">
+            Não tem uma conta?{' '}
+            <Link href="#" className="underline">
+              Cadastrar
+            </Link>
+          </div>
+        </div>
+      </div>
+      <div className="bg-muted hidden lg:block">
+        <Image
+          src="/placeholder.svg"
+          alt="Image"
+          width="1920"
+          height="1080"
+          className="h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+        />
+      </div>
+    </div>
+  )
+}
