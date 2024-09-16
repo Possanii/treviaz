0fb0a19e91b90f9f7334f53103549e57b3ef3a90
Possanii <gpossanii@outlook.com>
2024-09-16 20:46:40 -0300

    feat: sign-up page

--- apps/web/src/app/(app)/auth/sign-up/page.tsx
+++ apps/web/src/app/(app)/auth/sign-up/page.tsx
@@ -7,12 +7,21 @@ import Link from 'next/link'
 export default function SignInPage() {
   return (
     <div className="w-full lg:grid lg:min-h-[600px] lg:grid-cols-2 xl:min-h-screen">
+      <div className="bg-muted hidden lg:block">
+        <Image
+          src="/placeholder.svg"
+          alt="Image"
+          width="1920"
+          height="1080"
+          className="h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+        />
+      </div>
       <div className="flex items-center justify-center py-12">
         <div className="mx-auto grid w-[350px] gap-6">
           <div className="grid gap-2 text-center">
-            <h1 className="text-3xl font-bold">Login</h1>
+            <h1 className="text-3xl font-bold">Cadastrar</h1>
             <p className="text-muted-foreground text-balance">
-              Entre com seu email para acessar sua conta
+              Entre com suas informações para criar sua conta
             </p>
           </div>
           <div className="grid gap-4">
@@ -26,37 +35,24 @@ export default function SignInPage() {
               />
             </div>
             <div className="grid gap-2">
-              <div className="flex items-center">
-                <Label htmlFor="password">Senha</Label>
-                <Link
-                  href="/forgot-password"
-                  className="ml-auto inline-block text-sm underline"
-                >
-                  Esqueceu sua senha?
-                </Link>
-              </div>
+              <Label htmlFor="password">Senha</Label>
               <Input id="password" type="password" required />
             </div>
+            <div className="grid gap-2">
+              <Label htmlFor="confirm-password">Confirmar senha</Label>
+              <Input id="confirm-password" type="password" required />
+            </div>
             <Button type="submit" className="w-full">
-              Login
+              Cadastrar
             </Button>
           </div>
           <div className="mt-4 text-center text-sm">
-            Não tem uma conta?{' '}
-            <Link href="/auth/sign-up" className="underline">
-              Cadastrar
+            Já possui uma conta?{' '}
+            <Link href="/auth/sign-in" className="underline">
+              Login
             </Link>
           </div>
         </div>
-      </div>
-      <div className="bg-muted hidden lg:block">
-        <Image
-          src="/placeholder.svg"
-          alt="Image"
-          width="1920"
-          height="1080"
-          className="h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
-        />
       </div>
     </div>
   )
