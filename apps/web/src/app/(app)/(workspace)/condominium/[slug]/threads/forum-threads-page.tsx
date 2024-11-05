bdcb13521dd8f86400975e4d2729f1fbb06b25b5
Possanii <gpossanii@outlook.com>
2024-11-05 19:38:04 -0300

    feat: add button to open modal to create a new post

--- apps/web/src/app/(app)/(workspace)/condominium/[slug]/threads/forum-threads-page.tsx
+++ apps/web/src/app/(app)/(workspace)/condominium/[slug]/threads/forum-threads-page.tsx
@@ -7,11 +7,12 @@ import {
 } from '@tanstack/react-query'
 import { ForumThreadsSkeleton } from '@treviaz/ui/components/custom/blog-skeleton'
 import { Button } from '@treviaz/ui/components/ui/button'
-import { CirclePlus } from 'lucide-react'
+import { CirclePlus, PlusCircle } from 'lucide-react'
 import { useParams } from 'next/navigation'
 import React from 'react'
 
 import { useModalCreateForumCategory } from '@/contexts/create-forum-category-modal-context'
+import { useModalCreateForumThread } from '@/contexts/create-forum-thread-modal-context'
 import { useQueryGetAllCategoriesCondominium } from '@/hooks/react-query/queries/forum/get-all-categories-from-condominium'
 import { useQueryGetAllForumThreads } from '@/hooks/react-query/queries/forum/get-all-forum-threads'
 import { queryClient } from '@/lib/query-client'
@@ -26,6 +27,8 @@ export function ForumThreadsPage() {
   } = useSuspenseQuery(useQueryGetAllCategoriesCondominium({ slug }))
 
   const { toggleModal } = useModalCreateForumCategory()
+  const { toggleModal: creteaForumThreadToggleModal } =
+    useModalCreateForumThread()
 
   queryClient.prefetchQuery(useQueryGetAllForumThreads({ slug: 'all' }))
   const dehydratedState = dehydrate(queryClient)
@@ -40,19 +43,30 @@ export function ForumThreadsPage() {
           Saiba mais sobre todas as novidades do nosso condominio.
         </p>
       </div>
-      <div className="mt-8 flex gap-4 overflow-x-auto pb-2">
-        {categories.map((category) => (
-          <Button
-            key={category.id}
-            variant={category.id === 'all' ? 'default' : 'outline'}
-          >
-            {category.name}
+      <div className="mt-8 flex overflow-x-auto pb-2 justify-between">
+        <div className="flex gap-4 max-w-[80%]">
+          {categories.map((category) => (
+            <Button
+              key={category.id}
+              variant={category.id === 'all' ? 'default' : 'outline'}
+            >
+              {category.name}
+            </Button>
+          ))}
+          <Button onClick={toggleModal}>
+            <CirclePlus className="size-4 mr-2" />
+            Criar categoria
           </Button>
-        ))}
-        <Button onClick={toggleModal}>
-          <CirclePlus className="size-4 mr-2" />
-          Criar categoria
-        </Button>
+        </div>
+        <div>
+          <Button
+            className="max-w-fit"
+            onClick={() => creteaForumThreadToggleModal({ categories })}
+          >
+            <PlusCircle className="mr-2 h-4 w-4" />
+            Criar um post
+          </Button>
+        </div>
       </div>
       <HydrationBoundary state={dehydratedState}>
         <React.Suspense fallback={<ForumThreadsSkeleton />}>
