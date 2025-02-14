dfcaa6f852b9c5829c98b57aa6ba05ee8225f131
Possanii <gpossanii@outlook.com>
2025-02-14 19:18:27 -0300

    config: add no-explicit-any on eslint rules

--- packages/eslint-config/node.js
+++ packages/eslint-config/node.js
@@ -16,6 +16,7 @@ module.exports = {
   },
   plugins: ['@typescript-eslint', 'simple-import-sort'],
   rules: {
+    '@typescript-eslint/no-explicit-any': 'warn',
     'prettier/prettier': [
       'error',
       {
