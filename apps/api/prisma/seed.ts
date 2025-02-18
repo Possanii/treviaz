2d52766bc88c8f4f083ed673503f7be5a8cc43ff
Possanii <gpossanii@outlook.com>
2025-02-18 19:46:33 -0300

    feat: improve seed to create users and condominium

--- apps/api/prisma/seed.ts
+++ apps/api/prisma/seed.ts
@@ -1,13 +1,49 @@
+import { faker } from '@faker-js/faker'
+
 import { prisma } from '../src/application/libs/prisma'
 
 async function seed() {
-  const condominiumId = 'cb9f4abf-841b-4bc4-8690-55736167c258'
-  const residentId = '2d4ae054-76f6-45e0-953f-f8ad08cf1f0f'
-
   // prisma.$transaction([
   //   prisma.financialCategory.deleteMany(),
   //   prisma.financialTransaction.deleteMany(),
   // ])
+
+  const owner = await prisma.user.create({
+    data: {
+      name: 'Treviaz',
+      email: 'treviaz@acme.com',
+      avatar_url: faker.image.avatar(),
+    },
+  })
+
+  const resident = await prisma.user.create({
+    data: {
+      name: 'John Doe',
+      email: 'johndoe@acme.com',
+      avatar_url: faker.image.avatar(),
+    },
+  })
+
+  const address = await prisma.address.create({
+    data: {
+      street: faker.location.street(),
+      number: faker.location.buildingNumber(),
+      city: faker.location.city(),
+      state: faker.location.state(),
+      zip_code: faker.location.zipCode(),
+      neighborhood: faker.location.continent(),
+      country: faker.location.country(),
+    },
+  })
+
+  const condominium = await prisma.condominium.create({
+    data: {
+      name: 'John Acme',
+      slug: 'john-acme',
+      owner_id: owner.id,
+      address_id: address.id,
+    },
+  })
 
   const categorie1 = await prisma.financialCategory.create({
     data: {
@@ -45,21 +81,21 @@ async function seed() {
     data: [
       {
         categoryId: categorie1.id,
-        condominiumId,
+        condominiumId: condominium.id,
         amount: 15000,
         status: 'PENDING',
         dueDate: new Date(),
       },
       {
         categoryId: categorie2.id,
-        condominiumId,
+        condominiumId: condominium.id,
         amount: 120,
         status: 'OVERDUE',
         dueDate: new Date(),
       },
       {
         categoryId: categorie3.id,
-        condominiumId,
+        condominiumId: condominium.id,
         amount: 150,
         status: 'PAID',
         paymentDate: new Date(),
@@ -67,19 +103,19 @@ async function seed() {
       },
       {
         categoryId: categorie4.id,
-        condominiumId,
+        condominiumId: condominium.id,
         amount: 540,
         status: 'PAID',
         paymentDate: new Date(),
         dueDate: new Date(new Date().setDate(new Date().getDate() - 5)), // less 3 days
-        residentId,
+        residentId: resident.id,
       },
     ],
   })
 
   const invoice1 = await prisma.invoice.create({
     data: {
-      residentId,
+      residentId: resident.id,
       totalAmount: 540,
       referenceMonth: new Date(),
     },
