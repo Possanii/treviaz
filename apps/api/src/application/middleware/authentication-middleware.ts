7757800c9218b945e66e03ae064c2e8b7b1e2d91
tfxsoftware <aswespeak.tfx@gmail.com>
2025-02-19 15:35:39 -0300

    feat: implement dynamic Keycloak public key retrieval for JWT verification

--- apps/api/src/application/middleware/authentication-middleware.ts
+++ apps/api/src/application/middleware/authentication-middleware.ts
@@ -1,4 +1,5 @@
 import { env } from '@treviaz/env'
+import axios from 'axios'
 import { verify } from 'jsonwebtoken'
 
 import { cookiesStorage } from '../../../../../packages/cookies'
@@ -9,6 +10,22 @@ import { IResponse } from '../interfaces/IResponse'
 import { IKeycloakJwtPayload, keycloakJwtSchema } from '../schemas/IKeycloakJwtPayload'
 
 export class AuthenticationMiddleware implements IMiddleware {
+  private publicKey: string | null = null
+
+  private async getPublicKey(): Promise<string> {
+    if (this.publicKey) return this.publicKey
+
+    const response = await axios.get(
+      `${env.KEYCLOAK_URL}/realms/${env.KEYCLOAK_REALM}/protocol/openid-connect/certs`
+    )
+    
+    // Get the first key from the JWKS
+    const key = response.data.keys[0]
+    this.publicKey = `-----BEGIN PUBLIC KEY-----\n${key.n}\n-----END PUBLIC KEY-----`
+    
+    return this.publicKey
+  }
+
   async handle({ cookies }: IRequest): Promise<IResponse | IData> {
     const authorization = cookies
 
@@ -18,7 +35,13 @@ export class AuthenticationMiddleware implements IMiddleware {
 
     try {
       const token = authorization[cookiesStorage.API_AUTH_TOKEN]
-      const decoded = verify(token, env.JWT_SECRET_KEY)
+      const publicKey = await this.getPublicKey()
+      
+      const decoded = verify(token, publicKey, {
+        algorithms: ['RS256'],
+        issuer: `${env.KEYCLOAK_URL}/realms/${env.KEYCLOAK_REALM}`
+      })
+
       const payload = keycloakJwtSchema.parse(decoded) as IKeycloakJwtPayload
 
       return {
@@ -26,7 +49,7 @@ export class AuthenticationMiddleware implements IMiddleware {
           user: payload,
         },
       }
-    } catch {
+    } catch (error) {
       throw new JwtError()
     }
   }
