440b50b87d5233f72f72c82eaaa62087ea9ccee1
tfxsoftware <aswespeak.tfx@gmail.com>
2025-02-26 08:48:04 -0300

    feat: Enhance resident and user services with role-based queries

--- apps/api/src/application/services/condominium/get-residents-from-condominium-by-slug-service.ts
+++ apps/api/src/application/services/condominium/get-residents-from-condominium-by-slug-service.ts
@@ -4,13 +4,30 @@ import { prisma } from '@/application/libs/prisma'
 
 export class GetResidentsFromCondominiumBySlugService {
   async execute({ slug }: { slug: string }): Promise<IresidentsTable> {
-    const residents = await prisma.condominium.findUnique({
+    const condominium = await prisma.condominium.findUnique({
       where: {
         slug,
       },
       select: {
         users: {
+          where: {
+            role: {
+              name: 'RESIDENT'
+            }
+          },
           select: {
+            joined_at: true,
+            role: {
+              select: {
+                id: true,
+                name: true,
+                permissions: {
+                  select: {
+                    name: true
+                  }
+                }
+              },
+            },
             user: {
               select: {
                 id: true,
@@ -19,13 +36,19 @@ export class GetResidentsFromCondominiumBySlugService {
                 avatar_url: true,
               },
             },
-            joined_at: true,
-            role: true,
           },
         },
       },
     })
 
-    return { residents }
+    return {
+      residents: condominium ? {
+        users: condominium.users.map(userCondominium => ({
+          joined_at: userCondominium.joined_at,
+          role: userCondominium.role.name as "RESIDENT" | "ADMIN" | "SYNDIC" | "BILLING" | "SERVICES",
+          user: userCondominium.user
+        }))
+      } : null
+    }
   }
 }
--- apps/api/src/application/services/dashboard/get-new-residents-by-month-service.ts
+++ apps/api/src/application/services/dashboard/get-new-residents-by-month-service.ts
@@ -2,20 +2,22 @@ import { prisma } from '@/application/libs/prisma'
 
 export class GetNewResidentsByMonthService {
   async execute({ slug }: { slug: string }) {
-    const totalNewresidents = await prisma.userCondominium.aggregate({
+    const totalNewResidents = await prisma.userCondominium.aggregate({
       _count: { _all: true },
       where: {
         condominium: {
           slug,
         },
-        role: 'RESIDENT',
-        // joined_at: {
-        //   gte: new Date(new Date().getFullYear(), new Date().getMonth(), 1),
-        //   lt: new Date(new Date().getFullYear(), new Date().getMonth() + 1, 1),
-        // },
+        role: {
+          name: 'RESIDENT'
+        },
+        joined_at: {
+          gte: new Date(new Date().getFullYear(), new Date().getMonth(), 1),
+          lt: new Date(new Date().getFullYear(), new Date().getMonth() + 1, 1),
+        },
       },
     })
 
-    return totalNewresidents._count._all || 0
+    return totalNewResidents._count._all || 0
   }
 }
