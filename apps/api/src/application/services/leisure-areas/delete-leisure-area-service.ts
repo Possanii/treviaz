1180d2e5aa1059fae049ea9f47add5e547e0c8e3
tfxsoftware <aswespeak.tfx@gmail.com>
2025-03-26 13:04:18 -0300

    feat: add DeleteLeisureAreaService for leisure area deletion

---
+++ apps/api/src/application/services/leisure-areas/delete-leisure-area-service.ts
@@ -0,0 +1,9 @@
+import { prisma } from "@/application/libs/prisma";
+
+export class DeleteLeisureAreaService {
+  async execute(id: string) {
+    const leisureArea = await prisma.leisureArea.delete({
+      where: { id },
+    })
+  }
+}
